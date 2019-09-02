fbd34e353607225bb2234bb6d1d2d852030c0a70
finleysun <finleysun@sarahs-air-2.attlocal.net>
2019-09-01 23:10:52 -0400

    first attempt at complete code for A1 novice

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,36 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int totalcustomers = scan.nextInt();
 		
-	}
+		String[] names = new String[totalcustomers];
+		double[] totalcost = new double[totalcustomers];
+		String food = "";
+		
+		for (int i=0; i<totalcustomers; i++) {
+			
+			names[i] = scan.next().charAt(0) +". " + scan.next();
+		    
+			int numberitems = scan.nextInt();
+			
+			double sum = 0;
+			
+			for (int a=0; a<numberitems; a++) {
+				
+				int quantity = scan.nextInt();
+			    food = scan.next();
+			    sum += quantity * scan.nextDouble();
+	   
+	        }
+			totalcost[i] = sum;
+		}
+			
+		scan.close();
+		
+		for (int x=0; x<totalcustomers; x++) {
+			String stc = String.format("%.2f", totalcost[x]);
+			System.out.println(names[x] + ": " + stc);
+		}
+	
+    }
 }
